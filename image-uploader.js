b9ce711d195ce1202485fdf82e7000d33e667d1e
Gungor Kocak <gngr.kck@gmail.com>
2015-10-08 19:52:55 +0300

    Basic upload functionality with progress.

---
+++ image-uploader.js
@@ -0,0 +1,27 @@
+var ImageUploader = function (aws, bucket) {
+  this.AWS = AWS,
+  this.bucket = bucket,
+  this.partSize = 10 * 1024 * 512,
+  this.queueSize = 1;
+
+  console.log("AWS ", AWS);
+}
+
+ImageUploader.prototype.upload = function (file, onprogress, done) {
+  var uploader =
+    new this.AWS.S3.ManagedUpload({
+      partSize: this.partSize,
+      queueSize: this.queueSize,
+      params: {Bucket: this.bucket, Key: file.name, Body: file}
+    });
+
+  uploader.on('httpUploadProgress', function(event) {
+    file.progress = (event.loaded / event.total) * 100;
+
+    console.log ("file progress is happening", file);
+
+    if(typeof(onprogress) === 'function') onprogress.call(this, file, event);
+  });
+
+  uploader.send(done);
+}
